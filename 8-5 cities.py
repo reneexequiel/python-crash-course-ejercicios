4b7e6ff65cbf657cb830c8ce281a0b009efb9b05
reneexequiel <96835183+reneexequiel@users.noreply.github.com>
2026-05-31 16:53:23 -0400

    Update 8-5 cities.py

--- 8-5 cities.py
+++ 8-5 cities.py
@@ -3,6 +3,6 @@
 def describe_city(city, pais='chile'):
     mensaje = city.title() + ' se encuentra en ' + pais.title()
     print(mensaje)
-describe_city('arica')
+describe_city('sss')
 describe_city('puerto varas')
 describe_city('berlin', 'alemania')
